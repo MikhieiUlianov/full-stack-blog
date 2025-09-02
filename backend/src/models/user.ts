475b2c5b53e6c9fbcb5d2f6a8c56097bab74b1cc
Mkh <example@gmail.com>
2025-09-02 17:52:45 +0200

    define-models

---
+++ backend/src/models/user.ts
@@ -0,0 +1,21 @@
+import mongoose, { Schema } from "mongoose";
+
+const userSchema = new Schema(
+  {
+    username: {
+      type: String,
+      required: true,
+      unique: true,
+    },
+    img: {
+      type: String,
+    },
+    savedPosts: {
+      type: String,
+      default: [],
+    },
+  },
+  { timestamps: true }
+);
+
+export default mongoose.model("User", userSchema);
