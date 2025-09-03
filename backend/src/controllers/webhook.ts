5e98067410f1b33f39cb7999b7a26809d532f2d4
Mkh <example@gmail.com>
2025-09-03 15:31:21 +0200

    posts-&-webhook-controllers

---
+++ backend/src/controllers/webhook.ts
@@ -0,0 +1,65 @@
+import { Request, Response } from "express";
+import { Webhook } from "svix";
+
+import User from "../models/user.js";
+
+interface ClerkUserData {
+  id: string;
+  username?: string;
+  profile_img_url?: string;
+  email_addresses?: { email_address: string }[];
+  email?: string;
+  first_name?: string;
+  last_name?: string;
+}
+
+interface SvixEvent<T = any> {
+  type: string;
+  data: T;
+  id: string;
+  version?: string;
+  created_at?: number;
+}
+
+export const clerkWebHook = async (req: Request, res: Response) => {
+  const WEBHOOK_SECRET = process.env.CLERK_WEBHOOK_SECRET;
+  if (!WEBHOOK_SECRET) throw new Error("Webhook secret needed!");
+
+  const payload = req.body;
+
+  const headers = req.headers as Record<string, string>;
+
+  const wh = new Webhook(WEBHOOK_SECRET);
+
+  let evt: SvixEvent<ClerkUserData>;
+
+  try {
+    evt = wh.verify(payload, headers) as SvixEvent<ClerkUserData>;
+  } catch {
+    return res.status(400).json({ message: "Webhook verification failed." });
+  }
+
+  const userData = {
+    username: evt.data.username || evt.data.email_addresses?.[0]?.email_address,
+    email: evt.data.email_addresses?.[0]?.email_address,
+    img: evt.data.profile_img_url,
+  };
+
+  if (evt.type === "user.created") {
+    const newUser = new User({
+      clerkUserId: evt.data.id,
+      ...userData,
+    });
+    await newUser.save();
+  }
+
+  /*   if (evt.type === "user.updated") {
+    await User.updateOne({ clerkUserId: evt.data.id }, { ...userData });
+  }
+ */
+  if (evt.type === "user.deleted") {
+    await User.deleteOne({ clerkUserId: evt.data.id });
+  }
+
+  res.status(200).json({ message: "Webhook received" });
+};
--- backend/src/index.ts
+++ backend/src/index.ts
@@ -1,18 +1,39 @@
 import dotenv from "dotenv";
-dotenv.config();
 
-import express from "express";
+import express, { NextFunction, Request, Response } from "express";
+import { clerkMiddleware } from "@clerk/express";
 
 import userRouter from "./routes/user.js";
 import postRouter from "./routes/post.js";
 import commentRouter from "./routes/comment.js";
+import webHooksRouter from "./routes/webhook.js";
+
 import connectDB from "./lib/connectDB.js";
 
+interface CustomError extends Error {
+  status?: number;
+}
+dotenv.config();
 const app = express();
+app.use(clerkMiddleware() as unknown as express.RequestHandler);
+app.use("/webhooks", webHooksRouter);
+app.use(express.json());
 
 app.use("/users", userRouter);
 app.use("/comments", commentRouter);
 app.use("/posts", postRouter);
+
+app.use(
+  (error: CustomError, req: Request, res: Response, next: NextFunction) => {
+    res.status(error.status || 500);
+
+    res.json({
+      message: error instanceof Error ? error.message : "Something went wrong",
+      status: error.status,
+      stack: error.stack,
+    });
+  }
+);
 
 app.listen(3000, () => {
   /*   connectDB(); */
